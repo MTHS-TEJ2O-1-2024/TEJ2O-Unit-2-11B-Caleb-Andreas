ab69e9f26739279df27ddec85a1f04da75ed2f7b
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-22 12:00:00 -0400

    continued code

--- main.ts
+++ main.ts
@@ -17,14 +17,22 @@ secondNumber = randint(0, 99)
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
+// Show firstNumber value when A button pressed.
 input.onButtonPressed(Button.A, function() {
-    
+    basic.clearScreen()
+    basic.showString("#1: " + (firstNumber).toString())
+    basic.showIcon(IconNames.Happy)
 })
 
+// Show secondNumber value when B button pressed.
 input.onButtonPressed(Button.B, function() {
-    
+    basic.clearScreen()
+    basic.showString("#2: " + (secondNumber).toString())
+    basic.showIcon(IconNames.Happy)
 })
 
+// Compare values of both of the variables.
 input.onGesture(Gesture.Shake, function() {
-    
+    if (firstNumber < secondNumber)
+        basic.showString((firstNumber).toString, "<", (secondNumber).toString)
 })

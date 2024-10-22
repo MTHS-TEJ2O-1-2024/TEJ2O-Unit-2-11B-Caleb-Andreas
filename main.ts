fcde20efcd4c80aaa43e7d8ac8e09428896c1c02
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-22 11:24:54 -0400

    Started comparing numbers code

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Oct 2024
+ * This program compares numbers.
 */
 
-basic.showString('Hello, World!')
+// Variables.
+let firstNumber: number
+let secondNumber: number
+
+// Set variables to be random
+firstNumber = randint(0, 99)
+secondNumber = randint(0, 99)
+
+// Happy at start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    
+})
+
+input.onButtonPressed(Button.B, function() {
+    
+})
+
+input.onGesture(Gesture.Shake, function() {
+    
+})
